f0d3f991dd53f6f852edbdcf50c0de98f1c34f41
Abhirajkar Bajpai <122393161+AbhirajkarBajpai@users.noreply.github.com>
2023-09-01 22:47:09 +0530

    Update Note.js

--- frontend/src/components/Pages/Comp/Note/Note.js
+++ frontend/src/components/Pages/Comp/Note/Note.js
@@ -1,1 +1,16 @@
 
+import React, { useState } from 'react';
+import Card from '../UI/Card';
+import NotesList from './NotesList';
+import './Note.css'
+
+
+function Note(props) {
+
+    return (
+        <Card className='notes' >
+            <NotesList items ={props.items} onUpdateNote={props.onUpdateNote} onRemoveNote={props.onRemoveNote}/>
+        </Card>);
+        
+}
+export default Note;
