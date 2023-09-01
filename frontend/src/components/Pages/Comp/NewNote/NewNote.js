842dd6e6876700fbd3d5d63375f0a368d9c1c164
Abhirajkar Bajpai <122393161+AbhirajkarBajpai@users.noreply.github.com>
2023-09-01 22:44:26 +0530

    Update NewNote.js

--- frontend/src/components/Pages/Comp/NewNote/NewNote.js
+++ frontend/src/components/Pages/Comp/NewNote/NewNote.js
@@ -1,1 +1,38 @@
 
+import React, { useState } from "react";
+import "./NewNote.css";
+import NoteForm from "./NoteForm";
+
+function NewNote(props) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function saveNoteDataHandler(enteredNoteData) {
+    const noteData = {
+      ...enteredNoteData,
+      // id: Math.random().toString(),
+    };
+    props.onAddNote(noteData);
+    setIsEditing(false);
+  }
+  function startEditingHandler() {
+    setIsEditing(true);
+  }
+  function stopEditingHandler() {
+    setIsEditing(false);
+  }
+
+  return (
+    <div className="new-note">
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Note</button>
+      )}
+      {isEditing && (
+        <NoteForm
+          onCancel={stopEditingHandler}
+          onSaveNoteData={saveNoteDataHandler}
+        />
+      )}
+    </div>
+  );
+}
+export default NewNote;
