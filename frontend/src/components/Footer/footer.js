f5e35fca1063094a3eb186bf6499b997c75a7132
Abhirajkar Bajpai <122393161+AbhirajkarBajpai@users.noreply.github.com>
2023-09-01 22:37:13 +0530

    Update footer.js

--- frontend/src/components/Footer/footer.js
+++ frontend/src/components/Footer/footer.js
@@ -1,1 +1,33 @@
+import React from 'react';
+import "./footer.css";
+
+function Footer() {
+    return (
+        <footer className="footer-container">
+          <div className="footer-content">
+            <div className="social-icons">
+              <a href="#" className="icon-link">
+                <i className="fab fa-github" />
+              </a>
+              <a href="#" className="icon-link">
+                <i className="fab fa-facebook" />
+              </a>
+              <a href="#" className="icon-link">
+                <i className="fab fa-twitter" />
+              </a>
+              <a href="#" className="icon-link">
+                <i className="fab fa-linkedin" />
+              </a>
+            </div>
+            <div className="author-info">
+              <p>Abhirajkar Bajpai</p>
+              <p>© {new Date().getFullYear()} NoteitPhy</p>
+            </div>
+          </div>
+        </footer>
+      );
+  }
+  
+
+export default Footer;
 
